958bc1caf923bacc07d545e9d9265d023edc22fa
BPYap <32807097+BPYap@users.noreply.github.com>
2020-04-18 11:55:56 +0800

    Add new lexicon entries

--- zh2sg.py
+++ zh2sg.py
@@ -12,8 +12,23 @@ Test sentences:
     1. 他 要 唱歌
     2. 他 不 会 唱歌 吗
     3. 我 和 他 追 一只 猫
-    4. 她 要 一只 白 猫 
-    5. 她 大概 会 知道
+    4. 她 要 一只 白 猫
+    5. 她 大概 知道
+    6. 那只 狗 追 一只 猫
+    7. 他 给 了 她
+    8. 他 给 了 她 一只 猫
+    9. 他 没 有 猫
+    10. 追 猫
+    11. 他 会 唱歌 吗
+    12. 我 和 她 和 他 追 一只 猫
+    13. 他 可以 吃 玻璃
+    14. 他们 欺负 她
+    15. 这个 在 睡觉
+    16. 全部 在 吃 东西
+    17. 她 觉得 他 不 会 唱歌
+    18. 那只 猫 很 可爱
+    19. 他 即将要 睡觉
+    20. 那只 猫 欺负 一只 大 蚂蚁
 
     (Singlish)
     1. He does eat the cat
@@ -26,13 +41,54 @@ Test sentences:
 import sys
 
 pred_map = {
-    '_cat_n_rel': '_cat_n_1_rel',
-    '_dog_n_rel': '_dog_n_1_rel',
     '"exist_q_rel"': 'exist_q_super_rel',
-    '_he_n_rel': '_he_n_1_rel',
-    # '_know_v_rel': '',
-    '_roughly_v_rel': 'roughly_rel',
-    '_can_v_1_rel': '_can_v_rel'
+    "_dog_n_rel": "_dog_n_1_rel",
+    "_cat_n_rel": "_cat_n_1_rel",
+    "_glass_n_rel": "_glass_n_1_rel",
+    "_thing_n_rel": "_thing_n_1_rel",
+    "_tree_n_rel": "_tree_n_1_rel",
+    "_ant_n_rel": "_ant_n_1_rel",
+    "_he_n_rel": "_he_n_1_rel",
+    "_she_n_rel": "_she_n_1_rel",
+    "_it_n_rel": "_it_n_1_rel",
+    "_I_n_rel": "_I_n_1_rel",
+    "_you_n_rel": "_you_n_1_rel",
+    "_they_n_rel": "_they_n_1_rel",
+    "_we_n_rel": "_we_n_1_rel",
+    # "_person_n_rel": "",
+    # "_Xiaoming_n_rel": "",
+    # "_there_n_rel": "",
+    # "_cute_a_rel": "_cute_a_rel",
+    # "_happy_a_rel": "_happy_a_rel",
+    # "_white_a_rel": "_white_a_rel",
+    # "_big_a_rel": "_big_a_rel",
+    # "_have_v_rel": "_have_v_rel",
+    # "_want_v_rel": "_want_v_rel",
+    # "_know_v_rel": "_know_v_rel",
+    # "_eat_v_rel": "_eat_v_rel",
+    # "_hurt_v_rel": "_hurt_v_rel",
+    # "_bully_v_rel": "_bully_v_rel",
+    # "_disturb_v_rel": "_disturb_v_rel",
+    # "_sing_v_rel": "_sing_v_rel",
+    # "_sleep_v_rel": "_sleep_v_rel",
+    # "_chase_v_rel": "_chase_v_rel",
+    # "_give_v_rel": "_give_v_rel",
+    "_think_v_rel": "_think_v_2_rel",
+    # "_ask_v_rel": "_ask_v_rel",
+    "_can_v_1_rel": "_can_v_rel",
+    "_can_v_2_rel": "_can_v_rel",
+    "_roughly_v_rel": "roughly_rel",
+    # "_will_v_rel": "_will_v_rel",
+    # "neg_rel": "neg_rel",
+    "_not_v_rel": "neg_rel",
+    # "_that_q_rel": "_that_q_rel",
+    # "_animal_n_rel": "",
+    # "_a_q_rel": "_a_q_rel",
+    # "_this_q_rel": "_this_q_rel",
+    # "_these_q_rel": "_these_q_rel",
+    # "_those_q_rel": "_those_q_rel",
+    # "_every_q_rel": "_every_q_rel",
+    # "_and_coord_rel": "_and_coord_rel",
 }
 
 def read_in():
@@ -42,7 +98,6 @@ def read_in():
         for k, v in pred_map.items():
             lines[i] = lines[i].replace(k, v)
 
-    #print lines
     return lines
 
 def main():
